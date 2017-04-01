bd008d6fbf5716741d4b0596fc4597005581a2a7
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-04-01 23:14:01 +0300

    getPercentageOfLongStrings

---
+++ functions/inc.func.getPercentageOfLongStrings.js
@@ -0,0 +1,36 @@
+const isValidType = require('../util');
+
+/**
+ * Returns the percentage of long words in a string or an array of strings.
+ * @param payload
+ * @param splitter
+ * @param minLength
+ * @returns {number}
+ */
+const getPercentageOfLongStrings = (payload = [], splitter = ' ', minLength = 16) => {
+  try {
+    if (
+      !isValidType(
+        [payload, splitter, minLength],
+        [['string', 'array'], ['string'], ['number']]
+      )
+    ) {
+      // Invalid payload or splitter.
+      console.error(
+        'Error [getPercentageOfLongStrings]: Invalid parameters'
+      );
+      return 0;
+    }
+    // Distinct words.
+    const parts = typeof payload === 'string'
+      ? payload.split(splitter)
+      : payload;
+    const longWords = parts.filter(x => x.length >= minLength);
+    return Math.round(longWords.length / (parts.length || 1) * 100);
+  } catch (e) {
+    console.error(`Error [getPercentageOfLongStrings]: ${e.message}`);
+    return 0;
+  }
+};
+
+module.exports = getPercentageOfLongStrings;
---
+++ tests/test.getPercentageOfLongStrings.js
@@ -0,0 +1,13 @@
+// nodeunit tests/test.getPercentageOfShortWords.js
+const getPercentageOfLongStrings = require('../functions/inc.func.getPercentageOfLongStrings');
+const strings = require('./strings.json')['getPercentageOfLongStrings'];
+
+// Tests --------------------------------------
+
+exports.all_strings_default_params = function (test) {
+  strings.forEach((strArr) => {
+    const result = getPercentageOfLongStrings(strArr[0]);
+    test.equal(result, strArr[1], strArr);
+  });
+  test.done();
+};
