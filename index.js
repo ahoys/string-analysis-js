b5e0ccdf3c49a668232af4ccac851dc631aea766
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-04-07 17:58:06 +0300

    Fixed invalid func name binding.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   getPercentageOfRepetitiveStructure: require('./functions/inc.func.getPercentageOfRepetitiveStructure'),
-  getPercentageOfShortWords: require('./functions/inc.func.getPercentageOfShortStrings'),
+  getPercentageOfShortStrings: require('./functions/inc.func.getPercentageOfShortStrings'),
   getPercentageOfLongStrings: require('./functions/inc.func.getPercentageOfLongStrings'),
   getPercentageOfRepetitiveChars: require('./functions/inc.func.getPercentageOfRepetitiveChars'),
 };
